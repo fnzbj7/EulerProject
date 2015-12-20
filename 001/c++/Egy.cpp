77f6c4f85af820b6ebce30fd108bf0af3b4167d4
fnzbj7 <jukijun91@gmail.com>
2015-12-20 22:17:00 +0100

    001 C++ Done
    Meg lett írva a c++ 001-es feladata

--- 001/c++/Egy.cpp
+++ 001/c++/Egy.cpp
@@ -3,9 +3,17 @@
 using namespace std;
 
 int main()
-{
-	int a =0;
-	cout << "Alma";
-	cin >> a;
-    return a+3;
+{	
+	int sum = 0;
+	int n = 1002/3;
+	cout << n << " " <<(n*(n-1))/2 << endl;
+	sum +=(3*n*(n-1))/2;
+	n = 1000/5;
+	cout << n << " " <<(n*(n-1))/2 << endl;
+	sum+=(5*n*(n-1))/2;
+	n=1005/15;
+	sum -=(15*n*(n-1))/2;
+	cout << sum;
+	
+    return 0;
 }
